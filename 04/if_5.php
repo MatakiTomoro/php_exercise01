e5b8c1ab0e0948fed669a8d434ed21bab7a9e5e4
MatakiTomoro <0719tomorrow@gmail.com>
2022-02-22 15:15:47 +0900

    制御構文1の課題

---
+++ 04/if_5.php
@@ -0,0 +1,17 @@
+<?php
+
+$num = 1;
+
+if ($num = 1) {
+    echo "1は素数ではありません";
+    exit;
+}
+
+for ($i = 2; $i < $num; $i++) {
+    if ($num % $i == 0) {
+        echo $num . "は素数ではありません";
+        exit;
+    }
+}
+
+echo $num . "は素数です";
