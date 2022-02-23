27cb5c6cd473ec74de30dc7f84065215e4a8f18e
MatakiTomoro <0719tomorrow@gmail.com>
2022-02-23 19:10:56 +0900

    制御構文2の課題再提出

--- 05/if2_1.php
+++ 05/if2_1.php
@@ -3,11 +3,11 @@
 $poket_money = 1000;
 $fund_raising = 101;
 
-echo 'あなたの所持金は1000円です。' . '<br>';
+echo 'あなたの所持金は' . $poket_money . '円です。' . '<br>';
 
 while ($poket_money >= $fund_raising) {
-    echo '101円募金しました。' . '<br>';
-    $poket_money = $poket_money - $fund_raising;
+    echo $fund_raising . '円募金しました。' . '<br>';
+    $poket_money -= $fund_raising;
     echo '残り残高は' . $poket_money . '円です。' . '<br>';
 }
 
--- 05/if2_5.php
+++ 05/if2_5.php
@@ -3,26 +3,30 @@
 $subjects = ['数学', '英語', '理科', '社会', '国語'];
 
 foreach ($subjects as $subject) {
-    echo $subject . 'の試験は';
     switch ($subject) {
         case '数学':
-            echo '明日です。' . '<br>';
+            $b = '明日';
             break;
 
         case '英語':
-            echo '明後日です。' . '<br>';
+            $b  = '明後日';
             break;
 
         case '理科':
-            echo '明後日です。' . '<br>';
+            $b  = '明後日';
             break;
 
         case '社会':
-            echo '昨日です。' . '<br>';
+            $b = '昨日';
             break;
 
         case '国語':
-            echo '中止です。' . '<br>';
+            $b = '中止';
             break;
+
+        
     }
+    echo $subject . 'の試験は' . $b . 'です。' . '<br>';
 }
+
+
